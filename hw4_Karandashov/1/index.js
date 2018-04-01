01983fd56a12521b8b15517fe3fc0a44fd282327
proffania <37771435+proffania@users.noreply.github.com>
2018-04-01 12:12:25 +0300

    Add files via upload

---
+++ hw4_Karandashov/1/index.js
@@ -0,0 +1,21 @@
+"USE STRICT";
+
+var obj = {
+    'единицы': 0,
+    'десятки': 0,
+    'сотни': 0,
+};
+
+function x(chislo) {
+    obj.единицы = chislo % 10;
+    obj.десятки = (parseInt(chislo / 10)) % 10;
+    obj.сотни = parseInt(chislo / 100);
+}
+
+var chislo = +prompt('Введите число от 0 до 999');
+if (chislo > 999)
+    console.log('Число больше 999!!!')
+else{
+    x(chislo);
+console.log(obj);
+}
